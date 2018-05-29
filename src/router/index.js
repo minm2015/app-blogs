22fd8b81cb07941f048ccdc31a8f952ae99ece7d
minmx <Zhangmengqin2018>
2018-05-29 20:30:56 +0800

    更新代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import home from '@/views/modules/home'
-import article from '@/views/modules/article'
-import articleDetail from '@/views/modules/article_detail'
-import timeline from '@/views/modules/timeline'
+import home from '@/views/home'
+import article from '@/views/article'
+import articleDetail from '@/views/article_detail'
+import timeline from '@/views/timeline'
 Vue.use(VueRouter)
 const routes = [
   { path: '/home', component: home },
